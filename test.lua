2f264fbc96f0bfaae70ee60c18e265414da8e20a
porserver <66151969+porserver@users.noreply.github.com>
2021-10-20 09:03:09 +0700

    Create test.lua

---
+++ test.lua
@@ -0,0 +1,4 @@
+local lib = loadstring(game:HttpGet('https://raw.githubusercontent.com/porserver/exoline/main/UILibrary', true))()
+local window = lib:CreateWindow('porserver', Vector2.new(460, 530), Enum.KeyCode.RightControl)
+local test = window:CreateTab("เทส")
+lib:Init()
